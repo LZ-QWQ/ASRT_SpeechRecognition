e86681642184ff809f7f22eefc57baa24000b2de
LZ_QWQ <lz1830837889@163.com>
2019-04-22 21:28:24 +0800

    4.22

--- train_mspeech.py
+++ train_mspeech.py
@@ -31,7 +31,7 @@ if(not os.path.exists(modelpath)): # 判断保存模型的目录是否存在
 
 system_type = plat.system() # 由于不同的系统的文件路径表示不一样，需要进行判断
 if(system_type == 'Windows'):
-	datapath = 'E:\\语音数据集'
+	datapath = 'dataset'
 	modelpath = modelpath + '\\'
 elif(system_type == 'Linux'):
 	datapath = 'dataset'
@@ -43,7 +43,8 @@ else:
 
 ms = ModelSpeech(datapath)
 
-#ms.LoadModel(modelpath + 'speech_model251_e_0_step_327500.model')
-ms.TrainModel(datapath, epoch = 50, batch_size = 16, save_step = 500)
+ms.LoadModel(modelpath + 'speech_model251_e_0_step_5000.model')
+ms.TrainModel(datapath, epoch = 50, batch_size = 8, save_step = 500)
 
 
+                                                                                                                             
